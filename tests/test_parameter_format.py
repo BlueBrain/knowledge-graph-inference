c487a28d19ff6a26b2119926bca589ece1faf903
mouffok <sarah.mouffok@epfl.ch>
2023-06-27 16:11:51 +0200

    test missing parameter value

--- tests/test_parameter_format.py
+++ tests/test_parameter_format.py
@@ -1,45 +1,77 @@
+import unittest
+
 from inference_tools.datatypes.query import Query, query_factory
+from inference_tools.exceptions.exceptions import InferenceToolsException
 from inference_tools.nexus_utils.bucket_configuration import NexusBucketConfiguration
 from inference_tools.source.source import DEFAULT_LIMIT
 from inference_tools.utils import format_parameters
 
 
-q0 = {
-    "@type": "SparqlQuery",
-    "hasBody": """""",
-    "hasParameter": [],
-    "queryConfiguration": {
-        "org": "bbp",
-        "project": "atlas",
-        "sparqlView": {
-            "@id": "https://bbp.epfl.ch/neurosciencegraph/data/views/aggreg-sp/dataset"
+class ParameterFormattingTest(unittest.TestCase):
+
+    def test_parameter_format_limit(self):
+        q0 = {
+            "@type": "SparqlQuery",
+            "hasBody": "",
+            "hasParameter": [],
+            "queryConfiguration": {
+                "org": "bbp",
+                "project": "atlas",
+            },
+            "resultParameterMapping": []
         }
-    },
-    "resultParameterMapping": []
-}
 
+        query: Query = query_factory(q0)
 
-def test_parameter_format_limit():
-    query: Query = query_factory(q0)
+        query_config_0 = query.query_configurations[0]
+        forge = NexusBucketConfiguration(query_config_0.org, query_config_0.project,
+                                         True).allocate_forge_session()
 
-    query_config_0 = query.query_configurations[0]
-    forge = NexusBucketConfiguration(query_config_0.org, query_config_0.project,
-                                     True).allocate_forge_session()
+        parameter_values = {}
+        limit, formatted_parameters = format_parameters(
+            query=query, parameter_values=parameter_values, forge=forge
+        )
 
-    parameter_values = {}
-    limit, formatted_parameters = format_parameters(
-        query=query, parameter_values=parameter_values, forge=forge
-    )
+        self.assertEqual(limit, DEFAULT_LIMIT)
 
-    assert limit == DEFAULT_LIMIT
+        limit_value = 50
+        parameter_values["LimitQueryParameter"] = limit_value
 
-    limit_value = 50
-    parameter_values["LimitQueryParameter"] = limit_value
+        limit2, formatted_parameters2 = format_parameters(
+            query=query, parameter_values=parameter_values, forge=forge
+        )
 
-    limit2, formatted_parameters2 = format_parameters(
-        query=query, parameter_values=parameter_values, forge=forge
-    )
+        self.assertEqual(limit2, limit_value)
 
-    assert limit2 == limit_value
+    def test_parameter_format_missing_mandatory(self):
+        q1 = {
+            "@type": "SparqlQuery",
+            "hasBody": "",
+            "hasParameter": [
+                {
+                    "@type": "sparql_list",
+                    "description": "test field",
+                    "name": "MandatoryField",
+                    "optional": False
+                }
+            ],
+            "queryConfiguration": {
+                "org": "bbp",
+                "project": "atlas"
+            },
+            "resultParameterMapping": []
+        }
 
+        query: Query = query_factory(q1)
+        query_config_0 = query.query_configurations[0]
+        forge = NexusBucketConfiguration(
+            query_config_0.org, query_config_0.project, True
+        ).allocate_forge_session()
+
+        parameter_values = {}
+
+        with self.assertRaises(InferenceToolsException):
+            limit, formatted_parameters = format_parameters(
+                query=query, parameter_values=parameter_values, forge=forge
+            )
 
